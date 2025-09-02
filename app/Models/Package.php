1653b27e5f09872cb9937cad1f112e08a4dab1c4
IzzahSN <izzahfsn@gmail.com>
2025-09-02 13:51:58 +0800

    `Added SoftDeletes, set primary key and fillable attributes to Package model`

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Package extends Model
 {
     /** @use HasFactory<\Database\Factories\PackageFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+    protected $primaryKey = 'package_id';
+
+    protected $fillable = [
+        'package_name',
+        'package_rate',
+        'duration_per_sessions',
+        'unit',
+        'details',
+        'session_per_week',
+    ];
 }
