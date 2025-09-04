029ad2e1fcab5b8b35bfb3c677912f7e8f7d31bb
IzzahSN <izzahfsn@gmail.com>
2025-09-04 14:08:32 +0800

    `Added PackageFactory and updated PackageSeeder to include factory usage and example data.`

--- database/factories/PackageFactory.php
+++ database/factories/PackageFactory.php
@@ -16,8 +16,18 @@ class PackageFactory extends Factory
      */
     public function definition(): array
     {
+        $durationOptions = ['30 minutes', '1 hour'];
+        $unitOptions = ['per month', 'per session'];
+
         return [
-            //
+            'package_name'        => $this->faker->words(2, true),
+            'package_type'        => $this->faker->randomElement(['personal', 'group']),
+            'status'              => $this->faker->randomElement(['active', 'inactive']),
+            'package_rate'        => $this->faker->randomFloat(2, 10, 500), // contoh RM10 - RM500
+            'duration_per_sessions' => $this->faker->randomElement($durationOptions),
+            'unit'                => $this->faker->randomElement($unitOptions),
+            'details'             => $this->faker->sentence(8),
+            'session_per_week'    => $this->faker->numberBetween(1, 5),
         ];
     }
 }
--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Package;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,30 @@ class PackageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // contoh data real sikit
+        Package::create([
+            'package_name'         => 'An-Nur Lite',
+            'package_type'         => 'personal',
+            'status'               => 'active',
+            'package_rate'         => 20,
+            'duration_per_sessions' => '30 minutes',
+            'unit'                 => 'per seesion',
+            'details'              => '1-to-1 personal learning, 30 minutes per session.',
+            'session_per_week'     => 2,
+        ]);
+
+        Package::create([
+            'package_name'         => 'An-Nur Pro',
+            'package_type'         => 'group',
+            'status'               => 'active',
+            'package_rate'         => 100,
+            'duration_per_sessions' => '1 hour',
+            'unit'                 => 'per month',
+            'details'              => 'Group learning session for up to 6 students.',
+            'session_per_week'     => 3,
+        ]);
+
+        // generate random data dengan factory
+        Package::factory()->count(5)->create();
     }
 }
