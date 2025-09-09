cbea72eeec4cfcb3f65803e5a6c286585ce515dd
IzzahSN <izzahfsn@gmail.com>
2025-09-09 23:11:47 +0800

    `Refactor admin record tutor view: update stats cards styles and labels`

--- resources/views/admin/record/tutor.blade.php
+++ resources/views/admin/record/tutor.blade.php
@@ -1,22 +1,22 @@
 <x-admin-layout :title="'Tutor'">
     <!-- Stats Cards -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
-        <div class="bg-white p-4 rounded-xl shadow flex items-center gap-3">
-            <div class="pl-3">
-                <p class="text-sm text-gray-500">Total Active Teachers</p>
-                <h3 class="text-2xl font-bold">8</h3>
+        <div class="bg-white p-4 rounded-sm shadow flex items-center gap-3 border-l-lime-600 border-l-6">
+            <div class="">
+                <p class="text-sm font-semibold text-lime-600 uppercase">Total Active Teachers</p>
+                <h3 class="text-2xl font-bold text-gray-500">8</h3>
             </div>
         </div>
-        <div class="bg-white p-4 rounded-xl shadow flex items-center gap-3">
-            <div class="pl-3">
-                <p class="text-sm text-gray-500">Total Inactive Teachers</p>
-                <h3 class="text-2xl font-bold">2</h3>
+        <div class="bg-white p-4 rounded-sm shadow flex items-center gap-3 border-l-amber-400 border-l-6">
+            <div class="">
+                <p class="text-sm font-semibold text-amber-600 uppercase">Total Inactive Teachers</p>
+                <h3 class="text-2xl font-bold text-gray-500">2</h3>
             </div>
         </div>
-        <div class="bg-white p-4 rounded-xl shadow flex items-center gap-3">
-            <div class="pl-3">
-                <p class="text-sm text-gray-500">Total Teachers</p>
-                <h3 class="text-2xl font-bold">10</h3>
+        <div class="bg-white p-4 rounded-sm shadow flex items-center gap-3 border-l-cyan-600 border-l-6">
+            <div class="">
+                <p class="text-sm font-semibold text-cyan-600 uppercase">Total Teachers</p>
+                <h3 class="text-2xl font-bold text-gray-500">10</h3>
             </div>
         </div>
     </div>
