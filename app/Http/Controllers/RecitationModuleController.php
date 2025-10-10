0db39537ebcea8bc4e038f42df2702e15348ec4b
IzzahSN <izzahfsn@gmail.com>
2025-10-11 00:34:28 +0800

    `Added validation and file upload functionality to RecitationModuleController, and removed module report view.`

--- app/Http/Controllers/RecitationModuleController.php
+++ app/Http/Controllers/RecitationModuleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\RecitationModule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class RecitationModuleController extends Controller
 {
@@ -17,19 +19,37 @@ class RecitationModuleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+            'recitation_name' => 'required|string|max:255',
+            'level_type' => 'required|string|max:255',
+            'first_page' => 'required|integer|min:1',
+            'end_page' => 'required|integer|min:1|gte:first_page',
+            'badge' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // max 2MB
+        ]);
+
+        // Handle file upload
+        if ($request->hasFile('badge')) {
+            $ext = $request->file('badge')->getClientOriginalExtension(); // ambil extension asal;
+            $fileName = 'badge_' . Str::slug($request->recitation_name) . '_' . time() . '.' . $ext;
+            $badgePath = $request->file('badge')->storeAs('badges', $fileName, 'public');
+        } else {
+            return back()->withErrors(['badge' => 'Badge upload failed.'])->withInput();
+        }
+
+        RecitationModule::create([
+            'recitation_name' => $request->recitation_name,
+            'level_type' => $request->level_type,
+            'first_page' => $request->first_page,
+            'end_page' => $request->end_page,
+            'badge' => $badgePath ?? null,
+        ]);
+
+        return redirect()->back()->with('success', 'Recitation Module created successfully.')->with('closeModalAdd', true);
     }
 
     /**
