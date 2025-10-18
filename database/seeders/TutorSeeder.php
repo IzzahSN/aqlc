655f82bb0534f0de056a7569ae2cab02a931f6db
IzzahSN <izzahfsn@gmail.com>
2025-10-18 20:32:19 +0800

    `Added hardcoded Tutor data and commented out factory creation`

--- database/seeders/TutorSeeder.php
+++ database/seeders/TutorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Tutor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class TutorSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class TutorSeeder extends Seeder
      */
     public function run(): void
     {
-        Tutor::factory(10)->create();
+        // Tutor::factory(10)->create();
+        Tutor::create([
+            'first_name' => 'Nurul Izzah',
+            'last_name'  => 'Binti Shahrul Nizam',
+            'username'   => 'Ustazah Izzah',
+            'ic_number'  => '030710101656',
+            'birth_date' => '2003-07-10',
+            'age'        => 22,
+            'gender'     => 'Female',
+            'phone_number'      => '0132788701',
+            'role'       => 'Admin',
+            'email'      => 'izzahfsn@gmail.com',
+            'status'     => 'active',
+            'password' => Hash::make('030710101656'),
+        ])
     }
 }
