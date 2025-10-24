a7d6145186388bc3d30abe2c4982e4f5afc11da9
IzzahSN <izzahfsn@gmail.com>
2025-10-24 10:23:17 +0800

    `Update favicon links to logo-1.svg across multiple layout files`

--- resources/views/components/sidebar-tutor.blade.php
+++ resources/views/components/sidebar-tutor.blade.php
@@ -8,7 +8,7 @@
 <aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
         <a href="{{ route('tutor.dashboard') }}" class=" flex items-center ps-2.5 mb-5">
-            <img src="/images/logo-round.svg" class="h-10 w-10 rounded-full shadow me-3" alt=" AQLC Logo" />
+            <img src="/images/logo-1.svg" class="sidebar-logo h-10 w-10 me-3" alt=" AQLC Logo" />
             <h2 class="self-center text-sm font-semibold">Pusat Pengajian Quran<br>As-Siraaj</h2>
         </a>
         <ul class="space-y-2 font-semibold">
