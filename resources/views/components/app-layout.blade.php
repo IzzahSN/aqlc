80318fcc8af1edb3aee117bc0404e4269f4f1dbb
IzzahSN <izzahfsn@gmail.com>
2025-10-31 19:15:16 +0800

    `Added AlpineJS and updated meta tags in app-layout.blade.php`

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/svg" href="/images/logo-1.svg">
     <title>PPQA</title>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
